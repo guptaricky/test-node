23d86aaa2baf989316c5bd329950af6fabd1ae21
guptaricky <33823326+guptaricky@users.noreply.github.com>
2025-02-28 14:29:01 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const jwtauthenticate = (req,res,next) => {
     const token = req.header('Authorization').split(" ")[1];
 
@@ -6,8 +8,9 @@ const jwtauthenticate = (req,res,next) => {
     }
 
     try {
-        const decoded = jwt.verify(process.env.JWT_TOKEN);
-        req.username = decoded
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        req.user = decoded // Attach decoded payload to req.user
+        next();
     } catch (error) {
         return res.status(401).json({"message":"Invalid Token"})
     }
